4c3c7571b2dc7cd2373ec8059251403e1fdc7596
Pierre Rineau <pierre.rineau@makina-corpus.com>
2017-11-24 17:31:00 +0100

    added #range options for yearly allowed ranges in selector

--- udate.js
+++ udate.js
@@ -2,6 +2,18 @@
 /*global jQuery, Drupal */
 (function ($) {
   "use strict";
+
+  function checkDateInAllowedRanges(date, ranges) {
+    var formatted = $.datepicker.formatDate('mm-dd', date);
+    for (var i in ranges) {
+      var current = ranges[i];
+      if (current[0] <= formatted && formatted <= current[1]) {
+        return [true];
+      }
+    }
+    return [false];
+  }
+
   Drupal.behaviors.udate = {
     attach: function (context) {
       var id, settings;
@@ -11,6 +23,16 @@
       for (id in Drupal.settings.udate) {
         settings = Drupal.settings.udate[id];
         if ("object" === typeof settings && settings.dateFormat) {
+
+          if ((settings.allowedRanges instanceof Array) && settings.allowedRanges.length) {
+            // jQuery.datepicker seems to drop the unknown options so we must
+            // keep a copy here.
+            var ranges = settings.allowedRanges;
+            settings.beforeShowDay = function (date) {
+              return checkDateInAllowedRanges(date, ranges);
+            };
+          }
+
           $(context).find('#' + id).once('udate', function () {
             $(this).datepicker(settings);
           });
